4436f91af362f95cb7171a9b7a29c41c777d11ff
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-02-02 23:12:24 +0900

    add codes

--- index.js
+++ index.js
@@ -1,26 +1,70 @@
-var _a, _b;
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var _a, _b, _c, _d;
 // window.AudioContext = window.AudioContext || window.webkitAudioContext;
-var ctx = new AudioContext();
-var audioElement = document.querySelector("audio");
+const ctx = new AudioContext();
+let sampleSource;
+let isPlaying = false;
+// 音源を取得しAudioBuffer形式に変換して返す関数
+const setupSample = () => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield fetch("./audio/se.mp3");
+    const arrayBuffer = yield response.arrayBuffer();
+    // Web Audio APIで使える形式に変換
+    const audioBuffer = yield ctx.decodeAudioData(arrayBuffer);
+    return audioBuffer;
+});
+// AudioBufferをctxに接続し再生する関数
+const playSample = (_ctx, audioBuffer) => {
+    sampleSource = _ctx.createBufferSource();
+    // 変換されたバッファーを音源として設定
+    sampleSource.buffer = audioBuffer;
+    // 出力につなげる
+    sampleSource.connect(_ctx.destination);
+    sampleSource.start();
+    isPlaying = true;
+};
+const playSe = () => __awaiter(void 0, void 0, void 0, function* () {
+    // 再生中なら二重に再生されないようにする
+    if (isPlaying)
+        return;
+    const sample = yield setupSample();
+    playSample(ctx, sample);
+});
+(_a = document.querySelector("#play-se")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", playSe);
+const stopSe = () => __awaiter(void 0, void 0, void 0, function* () {
+    sampleSource === null || sampleSource === void 0 ? void 0 : sampleSource.stop();
+    isPlaying = false;
+});
+// oscillatorを破棄し再生を停止する
+(_b = document.querySelector("#stop")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", stopSe);
+const audioElement = document.querySelector("audio");
 // Web Audio API内で使える形に変換
 if (audioElement !== null) {
-    var track_1 = ctx.createMediaElementSource(audioElement);
-    var playAudio = function () {
+    const track = ctx.createMediaElementSource(audioElement);
+    const playAudio = () => {
         if (ctx.state === "suspended") {
-            ctx.resume()["catch"](function () {
+            ctx.resume().catch(() => {
                 console.log("resume");
             });
         }
         // 出力につなげる
-        track_1.connect(ctx.destination);
-        audioElement.play()["catch"](function () {
+        track.connect(ctx.destination);
+        audioElement.play().catch(() => {
             console.log("play");
         });
     };
-    (_a = document.querySelector("#play")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", playAudio);
-    var pauseAudio = function () {
+    (_c = document.querySelector("#play-music")) === null || _c === void 0 ? void 0 : _c.addEventListener("click", playAudio);
+    const pauseAudio = () => {
         audioElement === null || audioElement === void 0 ? void 0 : audioElement.pause();
     };
     // audioElementを一時停止する
-    (_b = document.querySelector("#pause")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", pauseAudio);
+    (_d = document.querySelector("#pause-music")) === null || _d === void 0 ? void 0 : _d.addEventListener("click", pauseAudio);
 }
--- index.ts
+++ index.ts
@@ -1,6 +1,46 @@
 // window.AudioContext = window.AudioContext || window.webkitAudioContext;
 const ctx = new AudioContext();
 
+let sampleSource: AudioBufferSourceNode;
+let isPlaying = false;
+
+// 音源を取得しAudioBuffer形式に変換して返す関数
+const setupSample = async (): Promise<AudioBuffer> => {
+  const response = await fetch("./audio/se.mp3");
+  const arrayBuffer = await response.arrayBuffer();
+  // Web Audio APIで使える形式に変換
+  const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
+  return audioBuffer;
+};
+
+// AudioBufferをctxに接続し再生する関数
+const playSample = (_ctx: AudioContext, audioBuffer: AudioBuffer): void => {
+  sampleSource = _ctx.createBufferSource();
+  // 変換されたバッファーを音源として設定
+  sampleSource.buffer = audioBuffer;
+  // 出力につなげる
+  sampleSource.connect(_ctx.destination);
+  sampleSource.start();
+  isPlaying = true;
+};
+
+const playSe = async (): Promise<void> => {
+  // 再生中なら二重に再生されないようにする
+  if (isPlaying) return;
+  const sample = await setupSample();
+  playSample(ctx, sample);
+};
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+document.querySelector("#play-se")?.addEventListener("click", playSe);
+
+const stopSe = async (): Promise<void> => {
+  sampleSource?.stop();
+  isPlaying = false;
+};
+// oscillatorを破棄し再生を停止する
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+document.querySelector("#stop")?.addEventListener("click", stopSe);
+
 const audioElement = document.querySelector("audio");
 // Web Audio API内で使える形に変換
 if (audioElement !== null) {
@@ -19,11 +59,11 @@ if (audioElement !== null) {
     });
   };
 
-  document.querySelector("#play")?.addEventListener("click", playAudio);
+  document.querySelector("#play-music")?.addEventListener("click", playAudio);
 
   const pauseAudio = (): void => {
     audioElement?.pause();
   };
   // audioElementを一時停止する
-  document.querySelector("#pause")?.addEventListener("click", pauseAudio);
+  document.querySelector("#pause-music")?.addEventListener("click", pauseAudio);
 }
