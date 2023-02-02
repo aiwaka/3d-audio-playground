1d7fcdd8531c03e83d3d60e229998bc436c512b5
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-02-02 22:04:35 +0900

    write small ts

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-function outputHello(text) {
-    console.log("Hello ".concat(text, "!"));
+var _a, _b;
+// window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var ctx = new AudioContext();
+var audioElement = document.querySelector("audio");
+// Web Audio API内で使える形に変換
+if (audioElement !== null) {
+    var track_1 = ctx.createMediaElementSource(audioElement);
+    var playAudio = function () {
+        if (ctx.state === "suspended") {
+            ctx.resume()["catch"](function () {
+                console.log("resume");
+            });
+        }
+        // 出力につなげる
+        track_1.connect(ctx.destination);
+        audioElement.play()["catch"](function () {
+            console.log("play");
+        });
+    };
+    (_a = document.querySelector("#play")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", playAudio);
+    var pauseAudio = function () {
+        audioElement === null || audioElement === void 0 ? void 0 : audioElement.pause();
+    };
+    // audioElementを一時停止する
+    (_b = document.querySelector("#pause")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", pauseAudio);
 }
-var textWorld = "World";
-outputHello(textWorld);
--- index.ts
+++ index.ts
@@ -1,7 +1,29 @@
-function outputHello(text: string): void {
-  console.log(`Hello ${text}!`);
+// window.AudioContext = window.AudioContext || window.webkitAudioContext;
+const ctx = new AudioContext();
+
+const audioElement = document.querySelector("audio");
+// Web Audio API内で使える形に変換
+if (audioElement !== null) {
+  const track = ctx.createMediaElementSource(audioElement);
+
+  const playAudio = (): void => {
+    if (ctx.state === "suspended") {
+      ctx.resume().catch(() => {
+        console.log("resume");
+      });
+    }
+    // 出力につなげる
+    track.connect(ctx.destination);
+    audioElement.play().catch(() => {
+      console.log("play");
+    });
+  };
+
+  document.querySelector("#play")?.addEventListener("click", playAudio);
+
+  const pauseAudio = (): void => {
+    audioElement?.pause();
+  };
+  // audioElementを一時停止する
+  document.querySelector("#pause")?.addEventListener("click", pauseAudio);
 }
-
-const textWorld: string = "World";
-
-outputHello(textWorld);
