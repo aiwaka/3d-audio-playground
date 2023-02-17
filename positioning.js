e084f653293b566db6f7f31406cfaf04b0d7f71a
Aiwaka <Kw.I.KU.1414@gmail.com>
2023-02-17 11:39:49 +0900

    face shape

--- positioning.js
+++ positioning.js
@@ -11,11 +11,18 @@ const getCanvasRenderingContext2D = (id) => {
     return null;
 };
 window.addEventListener("load", () => {
+    // 顔っぽい線を描く
     const elevCtx = getCanvasRenderingContext2D("elev-canvas");
     if (elevCtx !== null) {
         elevCtx.beginPath();
-        elevCtx.moveTo(0, 0);
-        elevCtx.lineTo(100, 100);
+        elevCtx.moveTo(50, 0);
+        elevCtx.bezierCurveTo(40, 0, 20, 20, 20, 40);
+        elevCtx.lineTo(10, 60);
+        elevCtx.lineTo(18, 65);
+        elevCtx.lineTo(25, 80);
+        elevCtx.bezierCurveTo(30, 90, 30, 90, 50, 85);
+        elevCtx.moveTo(40, 40);
+        elevCtx.arc(50, 50, Math.sqrt(200), (Math.PI / 180) * -135, (Math.PI / 180) * 135);
         elevCtx.stroke();
     }
 });
